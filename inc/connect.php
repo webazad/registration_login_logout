6512755d076ec0781077ab9ead884e26e2db8f95
Md. Abul Kalam Azad <webdevazad@gmail.com>
2018-03-10 03:04:50 +0600

    Update connect.php

--- inc/connect.php
+++ inc/connect.php
@@ -1,1 +1,18 @@
 
+<?php
+$db_host =  'localhost';
+$db_user = 'root';
+$db_name = 'registration_login_logout';
+$db_password = '';
+$connect = @mysqli_connect($db_host,$db_user,$db_password,$db_name);
+//$connect = new mysqli($db_host,$db_user,$db_password,$db_name);
+if(!$connect){
+    echo $connection_status = 'Something is wrong in database connection ...';
+    exit();
+    mysqli_get_host_info($connect);
+    mysqli_close($connect);
+}else{
+    $connection_status = null;
+}
+echo $connection_status;
+?>
